74f74517aa86511838b67fa62f4fb5b68c702658
Siddharth Mishra <siddharthmishra2709@gmail.com>
2026-04-06 21:10:49 +0530

    fixed e2e test included in vitest

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -10,5 +10,6 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['./src/test/setup.js'],
+    exclude: ['**/node_modules/**', '**/e2e/**'],
   },
 });
